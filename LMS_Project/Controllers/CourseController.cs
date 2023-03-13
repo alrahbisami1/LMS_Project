f48013eb2e837ef76c275e3ed8cc44f5aa299e55
user-14 <user-14@LAPTOP-BUPF2K83>
2023-03-13 09:16:03 +0400

    course controller

---
+++ LMS_Project/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_Project.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
