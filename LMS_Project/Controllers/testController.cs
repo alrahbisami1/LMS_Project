3f22257241da608eb4af83a90336fa86b7ce6418
User-18 <User-18@LAPTOP-32RCS9I5>
2023-03-13 08:55:29 +0400

    third commite

---
+++ LMS_Project/Controllers/testController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_Project.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
